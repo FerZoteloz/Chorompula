9d98628ac5c89586ee0dc4aff7c56301fb86df8a
Val <val.cb@ciencias.unam.mx>
2026-05-02 13:58:21 -0600

    configuracion funcional

--- proyecto_final_IS/accounts/views.py
+++ proyecto_final_IS/accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.views import LoginView
+from .forms import CustomLoginForm
+
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    authentication_form = CustomLoginForm
