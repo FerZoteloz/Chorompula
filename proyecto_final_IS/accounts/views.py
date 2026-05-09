8467918379ebb34a1a5f7c7c1d9d15d319643ea6
Val <val.cb@ciencias.unam.mx>
2026-05-09 12:46:57 -0600

    renderiza login form

--- proyecto_final_IS/accounts/views.py
+++ proyecto_final_IS/accounts/views.py
@@ -5,5 +5,5 @@ from django.contrib.auth.views import LoginView
 from .forms import CustomLoginForm
 
 class CustomLoginView(LoginView):
-    template_name = 'accounts/login.html'
+    template_name = 'accounts/index.html'
     authentication_form = CustomLoginForm
