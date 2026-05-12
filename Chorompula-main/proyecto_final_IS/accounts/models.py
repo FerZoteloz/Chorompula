1d07d81b1b1d9f9ebd0c7780eff75b0cd908cf12
Ferzzzotelo <155671573+FerZoteloz@users.noreply.github.com>
2026-05-11 22:10:13 -0600

    Prueba

---
+++ Chorompula-main/proyecto_final_IS/accounts/models.py
@@ -0,0 +1,89 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Profile(models.Model):
+    ROLE_CHOICES = (
+        ('admin', 'Administrador'),
+        ('teacher', 'Profesor'),
+        ('student', 'Estudiante'),
+    )
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES
+    )
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+    
+
+class Course(models.Model):
+    STATUS_CHOICES = (
+        ('borrador', 'Borrador'),
+        ('publicado', 'Publicado'),
+        ('cerrado', 'Cerrado'),
+    )
+
+    title = models.CharField(max_length=100)
+    language = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    flag = models.CharField(max_length=10, blank=True, default="📘")
+
+    week = models.IntegerField(default=1)
+    progress = models.IntegerField(default=0)
+
+    creator = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="created_courses",
+        null=True,
+        blank=True
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="borrador"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class UsuarioCurso(models.Model):
+    ROL_CURSO_CHOICES = (
+        ('teacher', 'Profesor'),
+        ('student', 'Estudiante'),
+    )
+
+    usuario = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    curso = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE
+    )
+
+    rol_en_curso = models.CharField(
+        max_length=20,
+        choices=ROL_CURSO_CHOICES
+    )
+
+    fecha_asignacion = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('usuario', 'curso', 'rol_en_curso')
+
+    def __str__(self):
+        return f"{self.usuario.username} - {self.curso.title} - {self.rol_en_curso}"
